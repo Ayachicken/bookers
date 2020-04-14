6c1bb93b712c53ba344486e4e3f37a1820aa82cb
Ayachicken <digimonmon.a@gmail.com>
2020-04-15 00:28:16 +0900

    [課題2]ルートページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,30 @@
 class BooksController < ApplicationController
+  def route
+  	book = Book.all
+  end
+
   def index
+  	@books = Book.all
   end
 
   def show
+  	@book = Book.find(params[:id])
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def new
+  end
+
+  def update
   end
 
   def edit
   end
+
+  def destroy
+  end
+
 end
