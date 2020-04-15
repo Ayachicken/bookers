eccf3ace9dd565df0ef6784c3eb527d420f37e80
Ayachicken <digimonmon.a@gmail.com>
2020-04-15 21:45:31 +0900

    [add]create,edit,destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,26 +5,41 @@ class BooksController < ApplicationController
 
   def index
   	@books = Book.all
+  	@book = Book.new
   end
 
   def show
   	@book = Book.find(params[:id])
   end
 
+  def new
+  	@book = Book.find([:id])
+  end
+
+  def create
+  	book = Book.new(book_params)
+  	book.save
+  	redirect_to books_path
+  end
+
   def edit
   	@book = Book.find(params[:id])
   end
 
-  def new
-  end
-
-  def update
-  end
 
   def edit
+  	@book = Book.find(params[:id])
   end
 
   def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	redirect_to books_path
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title,:body)
   end
 
 end
